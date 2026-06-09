881d10a0eac95f26548dacd8a4e4367639aa22af
mucelep <celepm82@gmail.com>
2026-06-09 07:29:51 +0300

    fix

--- level_04/ft_list_remove_if/ft_list_remove_if.c
+++ level_04/ft_list_remove_if/ft_list_remove_if.c
@@ -4,7 +4,6 @@ void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
 	t_list *tmp;
 
-	tmp = *begin_list;
 	while (*begin_list)
 	{
 		if (!cmp((*begin_list)->data, data_ref))
